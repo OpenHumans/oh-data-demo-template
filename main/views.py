94499713082712c20e325038e444df28c67d59c9
Stephanie Unna <steph.unna@gmail.com>
2018-05-25 15:10:02 +0100

    Adding in dashboard and related views

--- main/views.py
+++ main/views.py
@@ -45,21 +45,118 @@ def complete(request):
         login(request, user,
               backend='django.contrib.auth.backends.ModelBackend')
 
-        # Initiate a data transfer task, then render `complete.html`.
-        xfer_to_openhumans.delay(oh_id=oh_member.oh_id)
-        
-        # Example HTTP get with requests_respectful via celery (in tasks.py)
-        # Required arguments are url and realms, but you may pass ANY additional
-        # arguments that the "requests" library accepts and they will work as well.
-        # make_request_respectful_get.delay('urlhere', realms=["Source"])
-
         context = {'oh_id': oh_member.oh_id,
                    'oh_proj_page': settings.OH_ACTIVITY_PAGE}
-        return render(request, 'main/complete.html',
-                      context=context)
+        if not hasattr(oh_member, 'datasourcemember'):
+            return render(request, 'main/complete.html',
+                          context=context)
+        return redirect("/dashboard")
 
     logger.debug('Invalid code exchange. User returned to starting page.')
     return redirect('/')
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        if hasattr(request.user.oh_member, 'datasourcemember'):
+            datasource_member = request.user.oh_member.datasourcemember
+            download_file = get_datasource_file(request.user.oh_member)
+            if download_file == 'error':
+                logout(request)
+                return redirect("/")
+            connect_url = ''
+            allow_update = check_update(datasource_member)
+        else:
+            allow_update = False
+            datasource_member = ''
+            download_file = ''
+            
+        context = {
+            'oh_member': request.user.oh_member,
+            'datasource_member': datasource_member,
+            'download_file': download_file,
+            'allow_update': allow_update
+        }
+        return render(request, 'main/dashboard.html',
+                      context=context)
+    return redirect("/")
+
+
+def remove_datasource(request):
+    if request.method == "POST" and request.user.is_authenticated:
+        try:
+            oh_member = request.user.oh_member
+            api.delete_file(oh_member.access_token,
+                            oh_member.oh_id,
+                            file_basename="dummy-data.json")
+            messages.info(request, "Your test account has been removed")
+            datasource_account = request.user.oh_member.datasourcemember
+            datasource_account.delete()
+        except:
+            datasource_account = request.user.oh_member.datasourcemember
+            datasource_account.delete()
+            messages.info(request, ("Something went wrong, please"
+                          "re-authorize us on Open Humans"))
+            logout(request)
+            return redirect('/')
+    return redirect('/dashboard')
+
+
+def update_data(request):
+    if request.method == "POST" and request.user.is_authenticated:
+        oh_member = request.user.oh_member
+        process_source.delay(oh_member.oh_id)
+        datasource_member = oh_member.datasourcemember
+        datasource_member.last_submitted = arrow.now().format()
+        datasource_member.save()
+        messages.info(request,
+                      ("An update of your data has been started! "
+                       "It can take some minutes before the first data is "
+                       "available. Reload this page in a while to find your "
+                       "data"))
+        return redirect('/dashboard')
+
+
+def datasource_complete(request):
+    """
+    Receive user from data source. Store data, start processing.
+    """
+    logger.debug("Received user returning from data source.")
+    # Exchange code for token.
+    # This creates an OpenHumansMember and associated user account.
+    ohmember = request.user.oh_member
+    datasource_member = get_datasource_member(ohmember=ohmember)
+
+    if datasource_member:
+        messages.info(request, "Your data source account has been connected")
+        process_source.delay(ohmember.oh_id)
+        return redirect('/dashboard')
+
+    logger.debug('Invalid code exchange. User returned to starting page.')
+    messages.info(request, ("Something went wrong, please try connecting your "
+                            "data source account again"))
+    return redirect('/dashboard')
+
+
+def get_datasource_member(ohmember):
+    """
+    Edit this function to create a data source member on return from
+    authentication with their API.
+    """
+    print("Returning from data source authentication and creating user model.")
+
+    OpenHumansMember
+
+    try:
+        datasource_member = DataSourceMember.objects.get(user=ohmember)
+        print('got old data source member')
+    except DataSourceMember.DoesNotExist:
+        datasource_member = DataSourceMember(user=datasource_member)
+        print('make new data source member')
+    datasource_member.save()
+
+    return datasource_member
+
 
 
 def oh_code_to_member(code):
